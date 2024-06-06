0fe200b20b85e6f4482ca0f85bbbd2f0a342eb82
rui <ruixue.fi@gmail.com>
2024-06-06 21:55:12 +0300

    #13 example succeeded

--- javax-jpa-hibernate/src/test/java/rx/AbstractITConfigTemplate.java
+++ javax-jpa-hibernate/src/test/java/rx/AbstractITConfigTemplate.java
@@ -1,24 +1,52 @@
 package rx;
 
+import com.zaxxer.hikari.HikariJNDIFactory;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.mariadb.jdbc.MariaDbDataSource;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import javax.naming.Context;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collections;
 
 public abstract class AbstractITConfigTemplate {
-    private MariaDBContainer<?> db;
+    private static MariaDBContainer<?> db;
+    private static EntityManagerFactory emf;
 
-    @BeforeEach
-    public void init() {
+    @BeforeAll
+    public static void initEnvironment() {
         db = new MariaDBContainer<>(DockerImageName.parse("mariadb:10.5.8"));
         db.setPortBindings(Collections.singletonList("3307:3306"));
         db.start();
+        System.setProperty("org.osjava.sj.root","src/test/resources/hikaricp");
+        System.setProperty(Context.OBJECT_FACTORIES, HikariJNDIFactory.class.getName());
+        emf = Persistence.createEntityManagerFactory("mariadb-test");
+
+    }
+    @AfterEach
+    public void truncateTables() throws SQLException {
+        String connectionUrl = "jdbc:mariadb://localhost:3307/test";
+        String user = "root";
+        String password = "test";
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, password);
+            Statement stmt = conn.createStatement()) {
+            stmt.execute("truncate table TestEntity");
+        }
     }
 
-    @AfterEach
-    public void destroy() {
+    @AfterAll
+    public static void destroy() {
+        emf.close();
         db.stop();
     }
 }
---
+++ javax-jpa-hibernate/src/test/java/rx/HibernateWithNarayanaJTAIT.java
@@ -0,0 +1,60 @@
+package rx;
+
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariJNDIFactory;
+import org.junit.jupiter.api.Test;
+import org.osjava.sj.SimpleJndiContextFactory;
+import rx.jpa.TestEntity;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.sql.DataSource;
+import javax.transaction.*;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HibernateWithNarayanaJTAIT extends AbstractITConfigTemplate {
+    @Test
+    public void testUserTransactionCommit() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mariadb-test");
+        UserTransaction tx = com.arjuna.ats.jta.UserTransaction.userTransaction();
+        tx.begin();
+        EntityManager em = emf.createEntityManager();
+        TestEntity e = new TestEntity();
+        em.persist(e);
+        tx.commit();
+        em.close();
+
+        EntityManager em2 = emf.createEntityManager();
+        List<TestEntity> result = em2.createQuery("select e from TestEntity e")
+                .getResultList();
+        assertFalse(result.isEmpty());
+        em2.close();
+        emf.close();
+    }
+    @Test
+    public void testUserTransactionRollback() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mariadb-test");
+        UserTransaction tx = com.arjuna.ats.jta.UserTransaction.userTransaction();
+        tx.begin();
+        EntityManager em = emf.createEntityManager();
+        TestEntity e = new TestEntity();
+        em.persist(e);
+        tx.rollback();
+        em.close();
+
+        EntityManager em2 = emf.createEntityManager();
+        List<TestEntity> result = em2.createQuery("select e from TestEntity e")
+                .getResultList();
+        assertTrue(result.isEmpty());
+        em2.close();
+        emf.close();
+    }
+}
