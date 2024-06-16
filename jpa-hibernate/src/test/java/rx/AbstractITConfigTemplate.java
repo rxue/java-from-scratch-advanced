1d8caa8f35158aad8c4c47bc0507db9bb75ec0ab
rui <ruixue.fi@gmail.com>
2024-06-16 14:01:18 +0300

    #14 added UserTransaction programmatic demarcation control on transaction and refactor a bit

--- jpa-hibernate/src/test/java/rx/AbstractITConfigTemplate.java
+++ jpa-hibernate/src/test/java/rx/AbstractITConfigTemplate.java
@@ -4,10 +4,16 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collections;
+import java.util.function.Consumer;
 
 public abstract class AbstractITConfigTemplate {
     private static MariaDBContainer<?> db;
@@ -19,6 +25,19 @@ public abstract class AbstractITConfigTemplate {
         db.setPortBindings(Collections.singletonList("3307:3306"));
         db.start();
         emf = Persistence.createEntityManagerFactory("mariadb-test");
+    }
+    @BeforeEach
+    protected void clean() {
+        executeInJDBCConnection(s -> s.execute("delete from TestEntity"));
+    }
+
+    protected static void executeInJDBCConnection(SQLStatementConsumer<Statement> consumer) {
+        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3307/test", "root", "test");
+             Statement stmt = conn.createStatement()) {
+            consumer.accept(stmt);
+        } catch (SQLException e) {
+            System.out.println("JDBC execution failure");
+        }
     }
 
     @AfterAll
--- jpa-hibernate/src/test/java/rx/MariaDBDataSourceBootstrapIT.java
+++ jpa-hibernate/src/test/java/rx/MariaDBDataSourceBootstrapIT.java
@@ -1,14 +1,62 @@
 package rx;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.Persistence;
+import jakarta.transaction.*;
 import org.junit.jupiter.api.Test;
+import rx.jpa.TestEntity;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.sql.ResultSet;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MariaDBDataSourceBootstrapIT extends AbstractITConfigTemplate {
     @Test
     public void bootstrapHibernateJPAWithDataSource() {
         assertNotNull(emf);
+    }
+    @Test
+    public void useEntityManagerAfterBootstrap() {
+        try (EntityManager em = emf.createEntityManager()) {
+            TestEntity newEntity = new TestEntity();
+            em.persist(newEntity);
+        }
+
+        executeInJDBCConnection(s -> {
+            ResultSet rs = s.executeQuery("select * from TestEntity");
+            assertFalse(rs.next());
+        });
+    }
+
+    @Test
+    public void useEntityManagerAfterBootstrap_withUserTransaction_commit() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        UserTransaction tx = com.arjuna.ats.jta.UserTransaction.userTransaction();
+        tx.begin();
+        try (EntityManager em = emf.createEntityManager()) {
+            TestEntity newEntity = new TestEntity();
+            em.persist(newEntity);
+        }
+        tx.commit();
+        executeInJDBCConnection(s -> {
+            ResultSet rs = s.executeQuery("select * from TestEntity");
+            assertTrue(rs.next());
+        });
+    }
+
+    @Test
+    public void useEntityManagerAfterBootstrap_withUserTransaction_rollback() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        UserTransaction tx = com.arjuna.ats.jta.UserTransaction.userTransaction();
+        tx.begin();
+        try (EntityManager em = emf.createEntityManager()) {
+            TestEntity newEntity = new TestEntity();
+            em.persist(newEntity);
+        }
+        tx.rollback();
+        executeInJDBCConnection(s -> {
+            ResultSet rs = s.executeQuery("select * from TestEntity");
+            assertFalse(rs.next());
+        });
     }
 }
