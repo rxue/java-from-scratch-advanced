865957af883d4ec5d3888d06f6aba8322b7bdf6a
rui <ruixue.fi@gmail.com>
2024-06-16 11:41:39 +0300

    #14 further refactor before actual coding

--- jpa-hibernate/src/test/java/rx/AbstractITConfigTemplate.java
+++ jpa-hibernate/src/test/java/rx/AbstractITConfigTemplate.java
@@ -2,25 +2,28 @@ package rx;
 
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.Collections;
 
 public abstract class AbstractITConfigTemplate {
-    private MariaDBContainer<?> db;
+    private static MariaDBContainer<?> db;
+    protected static EntityManagerFactory emf;
 
-    @BeforeEach
-    public void init() {
+    @BeforeAll
+    public static void init() {
         db = new MariaDBContainer<>(DockerImageName.parse("mariadb:10.5.8"));
         db.setPortBindings(Collections.singletonList("3307:3306"));
         db.start();
+        emf = Persistence.createEntityManagerFactory("mariadb-test");
     }
 
-    @AfterEach
-    public void destroy() {
+    @AfterAll
+    public static void destroy() {
+        emf.close();
         db.stop();
     }
 }
--- jpa-hibernate/src/test/java/rx/MariaDBDataSourceBootstrapIT.java
+++ jpa-hibernate/src/test/java/rx/MariaDBDataSourceBootstrapIT.java
@@ -7,13 +7,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MariaDBDataSourceBootstrapIT extends AbstractITConfigTemplate {
-    /**
-     * based on
-     *  - https://mariadb.com/kb/en/about-mariadb-connector-j/
-     */
     @Test
     public void bootstrapHibernateJPAWithDataSource() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mariadb-test");//, Collections.singletonMap("hibernate.connection.datasource",ds));
         assertNotNull(emf);
     }
 }
